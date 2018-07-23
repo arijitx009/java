80ae3b86224485bea49b536be73aac6608176cc6
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-24 01:26:01 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+//Implementing Vehicle properties
+class Vehicle {
+
+	private String vehicleName;
+	
+	//defining the start() of start
+	public void start()
+	{
+		System.out.println("Vehicle without config.");
+	}
+}
