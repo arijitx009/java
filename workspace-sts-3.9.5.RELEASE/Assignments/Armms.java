0fc7384dce341c0493f7c06876ee806611466f20
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-25 00:30:09 +0530

    Update Armms.java

--- workspace-sts-3.9.5.RELEASE/Assignments/Armms.java
+++ workspace-sts-3.9.5.RELEASE/Assignments/Armms.java
@@ -1,15 +1,15 @@
 class Arms
 {
-public static void main(String args[])
+public static void main(String args[]) 
 {
-int i,cpy,rem,res=0;
+int i,cpy,rem,res=0;  //taking data types and variables
 
-for(i=100;i<=999;i++){
+for(i=100;i<=999;i++){ //taking a loop from 100 to 999
 cpy=i;
 while(cpy>0)
 {
 
-rem=cpy%10;
+rem=cpy%10; //to check whether the number is armstrong or not
 res+=Math.pow(rem,3);
 cpy/=10;
 }
