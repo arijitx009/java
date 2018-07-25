e46c2310d4ff61fd63d04602075438c01fba68fc
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-25 23:28:23 +0530

    Update C.java

--- workspace-sts-3.9.5.RELEASE/ABCInterface/src/C.java
+++ workspace-sts-3.9.5.RELEASE/ABCInterface/src/C.java
@@ -1,11 +1,11 @@
 
-class C implements B {
+class C implements B { 
 	
-	public void display()
+	public void display()   		//overriding the display function and defining it
 	{
 		System.out.println("Hey");
 	}
-	public void display2()
+	public void display2()			//overriding the display2 function and defining it
 	{
 		System.out.println("Arijit");
 	}
