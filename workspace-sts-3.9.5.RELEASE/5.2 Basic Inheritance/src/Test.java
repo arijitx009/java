df1c30c98153572dd6854a97ef3d9e8450f6e3d6
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-25 23:09:47 +0530

    Update Test.java

--- workspace-sts-3.9.5.RELEASE/5.2 Basic Inheritance/src/Test.java
+++ workspace-sts-3.9.5.RELEASE/5.2 Basic Inheritance/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String args[])
 	{
 		Member.method1();								//Calling static method
-		Member memberobj = new Member();
+		Member memberobj = new Member();				//creating an object so as to call the non static method
 		memberobj.method2();							//Calling non-static method
 		
 	}
