940402084292abe1578d2576d8fe999ceaa569df
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-25 22:45:15 +0530

    Update First.java

--- workspace-sts-3.9.5.RELEASE/5.1 Basic Inheritance/src/First.java
+++ workspace-sts-3.9.5.RELEASE/5.1 Basic Inheritance/src/First.java
@@ -1,13 +1,13 @@
 
 public class First {
 	
-	int i=0;
-	First()
+	int i=0; //initializing int i
+	First() // constructor 
 	{
 		System.out.println("First Class");
 	}
 	
-	void disp()
+	void disp() // function 
 	{
 		System.out.println("Display of First class");
 	}
