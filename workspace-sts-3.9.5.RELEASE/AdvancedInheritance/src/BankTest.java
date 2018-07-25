de6700e86bb48ad54ed90cc41a367c0247f07095
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-26 00:19:43 +0530

    Update BankTest.java

--- workspace-sts-3.9.5.RELEASE/AdvancedInheritance/src/BankTest.java
+++ workspace-sts-3.9.5.RELEASE/AdvancedInheritance/src/BankTest.java
@@ -7,38 +7,38 @@ public class BankTest {
 		
 		MMBankFactory mm1 = new MMBankFactory();
 		
-		SavingAcc alfaizSavings = alfaiz.getNewSavingAccount("Arijit", 15200, 54000, true);//initailization of saving account
+		SavingAcc arijitSavings = arijit.getNewSavingAccount("Arijit", 15200, 54000, true);//initailization of saving account
 		System.out.println("After Initialization");
-		System.out.println(alfaizSavings);
+		System.out.println(arijitSavings);
 		System.out.println("");
 		
 		
-		CurrentAcc saifCurrent = saif.getNewCurrentAccount("saif", 15420, 54796, 10000);//initailization of Current account
+		CurrentAcc rohitCurrent = rohit.getNewCurrentAccount("rohit", 15420, 54796, 10000);//initailization of Current account
 		System.out.println("After Initialization");
-		System.out.println(saifCurrent);
+		System.out.println(rohitCurrent);
 		System.out.println("");
 		
 		
 		alfaizSavings.withdraw(42000);
 		System.out.println("After Withdrawal from savings"); //Withdrawal
-		System.out.println(alfaizSavings);
+		System.out.println(arijitSavings);
 		System.out.println("");
 		
 		
-		alfaizSavings.deposit(14520);
+		arijitSavings.deposit(14520);
 		System.out.println("After Deposit in Savings");//Deposit in saving account
-		System.out.println(alfaizSavings);
+		System.out.println(arijitSavings);
 		System.out.println("");
 		
 		
-		saifCurrent.withdraw(15640);
+		rohitCurrent.withdraw(15640);
 		System.out.println("After Withdrawal from Current");
-		System.out.println(saifCurrent);
+		System.out.println(rohitCurrent);
 		System.out.println("");
 
-		saifCurrent.withdraw(15640);
+		rohitCurrent.withdraw(15640);
 		System.out.println("After Deposit from Current");
-		System.out.println(saifCurrent);
+		System.out.println(rohitCurrent);
 		System.out.println("");
 		
 	}
