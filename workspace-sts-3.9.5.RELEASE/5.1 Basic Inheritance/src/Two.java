5d3f5320041f2fcbe9351c09ec2cdcf180d1b8f2
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-25 22:57:19 +0530

    Update Two.java

--- workspace-sts-3.9.5.RELEASE/5.1 Basic Inheritance/src/Two.java
+++ workspace-sts-3.9.5.RELEASE/5.1 Basic Inheritance/src/Two.java
@@ -1,13 +1,13 @@
 
-public class Two {
+public class Two {  //main class
 
 	
 	public static void main(String args[])
 	{
-		First f=new First();
-		f.disp();
-		Second s=new Second();
-		s.disp();
+		First f=new First(); //creating object of the first class
+		f.disp();//calling the display function
+		Second s=new Second(); //creating object of the second class
+		s.disp();//calling the display function
 		
 	}
 }
