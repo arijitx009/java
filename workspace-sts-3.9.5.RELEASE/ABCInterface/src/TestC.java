8378fd97d6682752c2bd25ceec283e1389ef673f
arijitx009 <41569612+arijitx009@users.noreply.github.com>
2018-07-26 00:04:21 +0530

    Update TestC.java

--- workspace-sts-3.9.5.RELEASE/ABCInterface/src/TestC.java
+++ workspace-sts-3.9.5.RELEASE/ABCInterface/src/TestC.java
@@ -2,9 +2,9 @@
 public class TestC {
 	public static void main(String args[]) 
 	{
-		C c=new C();
-		c.display();
-		c.display2();
+		C c=new C(); 		//creating an object for C
+		c.display();		//Calling the display1 function
+		c.display2();		//calling the display2 function
 		
 	}
 
